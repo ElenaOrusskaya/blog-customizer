cae5c98db1acccef245af80df4d645f97e4dd86f
Elena Orusskaya <elenaorowsski@gmail.com>
2025-06-12 17:41:17 +0300

    Реализована кастомизация блога

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,146 @@
-import { ArrowButton } from 'src/ui/arrow-button';
-import { Button } from 'src/ui/button';
-
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+import { FormEvent, useEffect, useRef } from 'react';
 
-export const ArticleParamsForm = () => {
+import { Text } from '../../ui/text/Text';
+import { Select } from '../../ui/select/Select';
+import { RadioGroup } from '../../ui/radio-group/RadioGroup';
+import { Separator } from '../../ui/separator/Separator';
+import { ArrowButton } from '../../ui/arrow-button/ArrowButton';
+import { Button } from '../../ui/button/Button';
+
+import { OptionType } from 'src/constants/articleProps';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+
+import { ArticleStyleSettings } from 'src/index';
+
+type Props = {
+	openState: boolean;
+	onToggleSidebar: () => void;
+	onCloseSidebar: () => void;
+	formState: ArticleStyleSettings;
+	onChange: (state: ArticleStyleSettings) => void;
+	onSubmit: () => void;
+	onReset: () => void;
+};
+
+export const ArticleParamsForm = ({
+	openState,
+	onToggleSidebar,
+	onCloseSidebar,
+	formState,
+	onChange,
+	onSubmit,
+	onReset,
+}: Props) => {
+	const formRef = useRef<HTMLElement | null>(null);
+
+	useEffect(() => {
+		function handleClickOutside(event: MouseEvent) {
+			const sidebar = formRef.current;
+			if (!sidebar) return;
+
+			if (openState && !sidebar.contains(event.target as Node)) {
+				onCloseSidebar();
+			}
+		}
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [openState, onCloseSidebar]);
+	// обновление одного поля
+	const handleSelectChange = (
+		field: keyof ArticleStyleSettings,
+		selected: OptionType
+	) => {
+		onChange({
+			...formState,
+			[field]: selected,
+		});
+	};
+
+	const handleSubmit = (evt: FormEvent) => {
+		evt.preventDefault();
+		onSubmit();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={openState} onClick={onToggleSidebar} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: openState,
+				})}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Text as='h1' size={31} weight={800} uppercase dynamicLite>
+						Задайте параметры
+					</Text>
+
+					<Select
+						title='шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(selected: OptionType) =>
+							handleSelectChange('fontFamilyOption', selected)
+						}
+					/>
+
+					<RadioGroup
+						title='размер шрифта'
+						name='font-size'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(selected: OptionType) =>
+							handleSelectChange('fontSizeOption', selected)
+						}
+					/>
+
+					<Select
+						title='цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(selected: OptionType) =>
+							handleSelectChange('fontColor', selected)
+						}
+					/>
+
+					<Separator />
+
+					<Select
+						title='цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(selected: OptionType) =>
+							handleSelectChange('backgroundColor', selected)
+						}
+					/>
+
+					<Select
+						title='ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(selected: OptionType) =>
+							handleSelectChange('contentWidth', selected)
+						}
+					/>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							type='clear'
+							htmlType='button'
+							onClick={onReset}
+						/>
+
+						<Button title='Применить' type='apply' htmlType='submit' />
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,78 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState, OptionType } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+
+export interface ArticleStyleSettings {
+	fontFamilyOption: OptionType;
+	fontSizeOption: OptionType;
+	fontColor: OptionType;
+	backgroundColor: OptionType;
+	contentWidth: OptionType;
+}
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [pageState, setPageState] =
+		useState<ArticleStyleSettings>(defaultArticleState);
+	const [formState, setFormState] =
+		useState<ArticleStyleSettings>(defaultArticleState);
+
+	function toggleSidebar() {
+		setIsSidebarOpen((prev) => {
+			if (!prev) {
+				setFormState(pageState);
+			}
+			return !prev;
+		});
+	}
+
+	function closeSidebar() {
+		setIsSidebarOpen(false);
+	}
+
+	function handleSubmit() {
+		setPageState(formState);
+		closeSidebar();
+	}
+
+	function handleReset() {
+		setFormState(defaultArticleState);
+		setPageState(defaultArticleState);
+	}
+
 	return (
-		<main
+		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': pageState.fontFamilyOption.value,
+					'--font-size': pageState.fontSizeOption.value,
+					'--font-color': pageState.fontColor.value,
+					'--container-width': pageState.contentWidth.value,
+					'--bg-color': pageState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				openState={isSidebarOpen}
+				onCloseSidebar={closeSidebar}
+				onToggleSidebar={toggleSidebar}
+				formState={formState}
+				onChange={setFormState}
+				onSubmit={handleSubmit}
+				onReset={handleReset}
+			/>
 			<Article />
-		</main>
+		</div>
 	);
 };
 
